966abe939b8679d2ec6a386d76a06cd85bf3d77f
j1nma <j1nma@users.noreply.github.com>
2020-07-19 18:51:51 +0200

    recipeService at RecipeList

--- recipe-book-app/src/app/recipes/recipe.service.ts
+++ recipe-book-app/src/app/recipes/recipe.service.ts
@@ -1,3 +1,10 @@
 export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe("A Test Recipe", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg"),
+        new Recipe("Another Test Recipe", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg")
+    ];
 
+    getRecipes() {
+        return this.recipes.slice(); // Return copy, not original recipes
+    }
 }
