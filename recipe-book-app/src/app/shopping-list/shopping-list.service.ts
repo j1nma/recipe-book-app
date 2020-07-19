9c6046c7b9e0fb19828d12035ee159cc5866fa46
j1nma <j1nma@users.noreply.github.com>
2020-07-19 19:17:58 +0200

    add ingredient working

--- recipe-book-app/src/app/shopping-list/shopping-list.service.ts
+++ recipe-book-app/src/app/shopping-list/shopping-list.service.ts
@@ -2,7 +2,7 @@ import { Ingredient } from '../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
-    ingredientAdded = new EventEmitter<Ingredient>();
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient("Apples", 5),
@@ -15,5 +15,6 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
