4f794c44108ec4d8960c55d85def54de77856461
j1nma <j1nma@users.noreply.github.com>
2020-07-19 19:23:24 +0200

    ingredients list

--- recipe-book-app/src/app/recipes/recipe.service.ts
+++ recipe-book-app/src/app/recipes/recipe.service.ts
@@ -1,12 +1,17 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe("A Test Recipe", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg"),
-        new Recipe("Another Test Recipe", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg")
+        new Recipe("Schnitzel", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg",
+            [new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)]),
+        new Recipe("Burger", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg",
+            [new Ingredient('Buns', 2),
+            new Ingredient('Meats', 1)])
     ];
 
     getRecipes() {
