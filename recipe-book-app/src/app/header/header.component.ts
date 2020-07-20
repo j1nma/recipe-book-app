ea9b60d9d6d1ac17c572ba1b44e369a5162cb460
j1nma <j1nma@users.noreply.github.com>
2020-07-20 21:19:13 +0200

    adding navigation

--- recipe-book-app/src/app/header/header.component.ts
+++ recipe-book-app/src/app/header/header.component.ts
@@ -1,22 +1,13 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
-
+export class HeaderComponent {
   collapsed = true;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 
 }
