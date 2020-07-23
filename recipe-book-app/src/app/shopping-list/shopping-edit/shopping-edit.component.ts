e5839687a7cb59ba6ae0d3e5fb26bb55673484de
j1nma <j1nma@users.noreply.github.com>
2020-07-23 02:31:57 +0200

    validating user input

--- recipe-book-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-book-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
-import { Subscription, VirtualTimeScheduler } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
