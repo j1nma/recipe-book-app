508c51ff5dac5cef98e0a1d5fc295fecb865b550
j1nma <j1nma@users.noreply.github.com>
2020-07-24 20:17:22 +0200

    sending the signup request

--- recipe-book-app/src/app/auth/auth.component.ts
+++ recipe-book-app/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
     selector: 'app-auth',
@@ -8,12 +9,32 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent {
     isLoginMode = true;
 
+    constructor(private authService: AuthService) { }
+
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
     }
 
     onSubmit(form: NgForm) {
+        if (!form.value) {
+            return; // If the user hacks through 'inspect elements'.
+        }
+        const email = form.value.email;
+        const password = form.value.password;
 
+        if (this.isLoginMode) {
+            //
+        } else {
+            this.authService
+                .signup(email, password)
+                .subscribe(
+                    resData => {
+
+                    },
+                    error => {
+                        console.log(error.message);
+                    });
+        }
         form.reset();
     }
 
