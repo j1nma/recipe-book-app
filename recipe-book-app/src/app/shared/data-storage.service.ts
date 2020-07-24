121cb3a2d91312a43ce95fdb8fe024f46f9d45fd
j1nma <j1nma@users.noreply.github.com>
2020-07-24 06:58:00 +0200

    storing recipes

--- recipe-book-app/src/app/shared/data-storage.service.ts
+++ recipe-book-app/src/app/shared/data-storage.service.ts
@@ -1,8 +1,22 @@
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 
 export class DataStorageService {
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient,
+        private recipeServices: RecipeService) { }
+
+    storeRecipes() {
+        const recipes = this.recipeServices.getRecipes();
+        // PUT overwrites everything
+        this.http
+            .put(
+                'https://ng-course-recipe-book-fcbcd.firebaseio.com/',
+                recipes
+            )
+            .subscribe();
+        // Here we don't need to subscribe from the triggering/calling component
+    }
 
 }
