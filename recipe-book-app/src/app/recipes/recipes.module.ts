68456bcba0a44ef6f58fecb69419707a93af9135
j1nma <j1nma@users.noreply.github.com>
2020-07-26 13:16:34 +0200

    adding routes to recipe module

--- recipe-book-app/src/app/recipes/recipes.module.ts
+++ recipe-book-app/src/app/recipes/recipes.module.ts
@@ -10,6 +10,7 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     imports: [
         RouterModule,
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RecipesRoutingModule
     ],
     exports: [
         RecipesComponent,
