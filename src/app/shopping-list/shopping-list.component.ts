44b78d629ab83709233cb03993a573f300f792cd
j1nma <j1nma@users.noreply.github.com>
2020-07-28 20:25:22 +0200

    expanding the state

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,7 +3,8 @@ import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service'
 import { Subscription, Observable } from 'rxjs';
 import { LoggingService } from '../logging.service';
-import { Store } from '@ngrx/store';
+import { Store, State } from '@ngrx/store';
+import * as fromShoppingList from './store/shopping-list.reducer';
 
 @Component({
   selector: 'app-shopping-list',
@@ -17,7 +18,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   constructor(
     private shoppingListService: ShoppingListService,
     private loggingService: LoggingService,
-    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+    private store: Store<fromShoppingList.AppState>
   ) { }
 
   ngOnInit(): void {
