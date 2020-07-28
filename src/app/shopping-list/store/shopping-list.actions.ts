3dbc95ba534c94b090cbf1bb6d6220dc49166e61
j1nma <j1nma@users.noreply.github.com>
2020-07-28 19:22:13 +0200

    setting up ngrx store

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -4,6 +4,6 @@ import { Ingredient } from '../../shared/ingredient.model';
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
 export class AddIngredient implements Action {
-    readonly type: string;
+    readonly type = ADD_INGREDIENT;
     payload: Ingredient;
 }
