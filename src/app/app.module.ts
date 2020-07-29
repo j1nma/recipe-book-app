6f101807e37a09ccddf420c6283ebf89e4971059
j1nma <j1nma@users.noreply.github.com>
2020-07-29 03:38:42 +0200

    effects & error handling, login via ngrx effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { LoggingService } from './logging.service';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthEffects } from './auth/store/auth.effects';
 import * as fromApp from './store/app.reducer';
 
 @NgModule({
@@ -21,6 +23,7 @@ import * as fromApp from './store/app.reducer';
     HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([AuthEffects]),
     SharedModule,
     CoreModule
   ],
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,10 +1,55 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { Actions, ofType, Effect } from '@ngrx/effects';
+import { switchMap, catchError, map } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import * as AuthActions from './auth.actions';
+import { of } from 'rxjs';
+import { Injectable } from '@angular/core';
 
+export interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+    registered?: boolean;
+}
+
+@Injectable()
 export class AuthEffects {
-    authLogin = this.actions$.pipe( // ngrx effects will subscribe for you
-        ofType(AuthActions.LOGIN_START)
-    );
 
-    constructor(private actions$: Actions) { }
+    constructor(
+        private actions$: Actions,
+        private http: HttpClient
+    ) { }
+
+    @Effect()
+    authLogin = this.actions$.pipe( // ngrx effects will subscribe and dispatch for you
+        ofType(AuthActions.LOGIN_START),
+        switchMap((authData: AuthActions.LoginStart) => {
+            return this.http
+                .post<AuthResponseData>(
+                    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey
+                    , {
+                        email: authData.payload.email,
+                        password: authData.payload.password,
+                        returnSecureToken: true,
+                    }
+                ).pipe(
+                    map(resData => {
+                        const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
+                        return of(new AuthActions.Login({
+                            email: resData.email,
+                            userId: resData.localId,
+                            token: resData.idToken,
+                            expirationDate: expirationDate
+                        }));
+                    }),
+                    catchError(error => {
+                        return of();
+                    }))
+        }),
+
+    );
 }
