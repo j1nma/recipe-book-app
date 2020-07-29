93591cee5f26ba843ee42571c6f17ce1c77d3faa
j1nma <j1nma@users.noreply.github.com>
2020-07-30 00:03:40 +0200

    fixing the auth redirect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -28,7 +28,8 @@ const handleAuthentication = (expiresIn: number, email: string, userId: string, 
         email: email,
         userId: userId,
         token: token,
-        expirationDate: expirationDate
+        expirationDate: expirationDate,
+        redirect: true
     });
 };
 
@@ -94,8 +95,8 @@ export class AuthEffects {
     @Effect({ dispatch: false })
     authRedirect = this.actions$.pipe(
         ofType(AuthActions.AUTHENTICATE_SUCCESS),
-        tap(() => {
-            this.router.navigate(['/']);
+        tap((authSuccessAction: AuthActions.AuthenticateSuccess) => {
+            if (authSuccessAction.payload.redirect) this.router.navigate(['/']);
         }));
 
     @Effect()
@@ -160,17 +161,15 @@ export class AuthEffects {
             );
 
             if (loadedUser.token) {
-                // this.user.next(loadedUser);
                 const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
                 this.authService.setLogoutTimer(expirationDuration);
                 return new AuthActions.AuthenticateSuccess({
                     email: loadedUser.email,
                     userId: loadedUser.id,
                     token: loadedUser.token,
-                    expirationDate: new Date(userData._tokenExpirationDate)
+                    expirationDate: new Date(userData._tokenExpirationDate),
+                    redirect: false
                 });
-                // const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
-                // this.autoLogout(expirationDuration);
             }
 
             return { type: 'DUMMY' }
